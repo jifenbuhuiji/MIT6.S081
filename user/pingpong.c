30073c0b71191e21f5f95414185b6f8bbaa94d3f
jifenbuhuiji <3349099382@qq.com>
2025-05-25 20:47:06 +0800

    pingpong

--- user/pingpong.c
+++ user/pingpong.c
@@ -1,11 +1,33 @@
 #include "kernel/types.h"
 #include "user/user.h"
 
-int main()
+int main(int argc, char *argv[])
 {
-    int pid = fork();
-    if(pid == 0)
+    int p[2];
+    pipe(p);
+    if(fork() != 0)
     {
-        
+        char buff[100];
+        write(p[1], "a", 1);
+        int status;
+        wait(&status);
+        int pid = getpid();
+        read(p[0], buff, 1);
+        printf("%d: received pong\n", pid);
+        close(p[0]);
+        close(p[1]);
+        exit(0);
     }
+    else
+    {
+        char buff[100];
+        int pid = getpid();
+        read(p[0], buff, 1);
+        write(p[1], buff, 1);
+        printf("%d: received ping\n", pid);
+        close(p[0]);
+        close(p[1]);
+        exit(0);
+    }
+    exit(0);
 }
